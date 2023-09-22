283cc91b6458752a63c5d8175383bfd58b49441e
dwgth4i <91010847+tydwg@users.noreply.github.com>
2023-09-22 09:36:34 +0700

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";  
-$username = "root";         
+$servername = "xxxx";  
+$username = "xxxx";         
 $password = "";             
-$database = "shopbanhang";  
+$database = "xxxx";  
 
 $conn = new mysqli($servername, $username, $password, $database);
 
