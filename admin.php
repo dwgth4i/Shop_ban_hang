e1c64f87972f7ee2316b62db3d56f4ee27ffe9b5
Trinh Thai Duong <91010847+dwgth4i@users.noreply.github.com>
2024-03-28 21:22:45 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,19 +1,24 @@
 <?php
-
 require "database.php";
+include "validation.php";
 
 if (isset($_POST["username"]) && isset($_POST["password"])) {
     $username = $_POST["username"];
     $password = $_POST["password"];
+    $enc_password = md5($password);
+    if (!validation($username,$password)) {
+        header("Location: youcantdoshit.php");
+        exit;
+    }
 
-    $stmt = $conn->prepare("SELECT * FROM users WHERE name = ? AND password= ?");
-    $stmt->bind_param("ss", $username, $password);
+    $stmt = $conn->prepare("SELECT username,password FROM users WHERE username=? AND password=?");
+    $stmt->bind_param("ss", $username, $enc_password);
     $stmt->execute();
     $result = $stmt->get_result();
     
     if ($result->num_rows === 1) {
         $row = $result->fetch_assoc();
-        if ($row["name"] === "admin"){
+        if ($row["username"] === "admin"){
             session_start();
             $_SESSION["admin"] = true;
             header("Location: admin_panel.php");
