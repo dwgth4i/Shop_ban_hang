458294ebf223648f28d5e9d8cccae4b6ced1146c
Trinh Thai Duong <91010847+dwgth4i@users.noreply.github.com>
2024-03-28 21:26:17 +0700

    Update register.php

--- register.php
+++ register.php
@@ -1,29 +1,55 @@
 <?php
 require "database.php";
+include 'validation.php';
 
 if (isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["email"])) {
     $username = $_POST["username"];
     $password = $_POST["password"];
     $confirm_password = $_POST["confirm_password"];
+    $enc_password = md5($confirm_password);
     $email = $_POST["email"];
+    if (!validation($username,$confirm_password)) {
+        header("Location: youcantdoshit.php");
+        exit;
+    }
     if (checkValid($conn, $username) && $confirm_password === $password) {
-        $sql = "INSERT INTO users(name, password, email) VALUES ('$username', '$password', '$email')";
-        $conn->query($sql);
-        echo "Đăng kí thành công";
+        $sql = "INSERT INTO users(name, password, email) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bind_param("sss", $username, $enc_password, $email);
+
+        if ($stmt->execute()) {
+            echo "Đăng kí thành công";
+        } else {
+            echo "Đăng kí không thành công. Vui lòng thử lại sau.";
+        }
     } else {
         echo "Vui lòng chọn tên đăng nhập khác, tên đăng nhập này đã bị trùng hoặc mật khẩu xác nhận không đúng với mật khẩu";
     }
 }
 
 function checkValid($conn, $username) {
-    $sql = "SELECT * FROM `users` WHERE name='$username'";
-    $result = $conn->query($sql);
+
+
+    $sql = "SELECT * FROM `users` WHERE name=?";
+    $stmt = $conn->prepare($sql);
+
+
+    $stmt->bind_param("s", $username);
+
+
+    $stmt->execute();
+
+
+    $result = $stmt->get_result();
+
     if ($result->num_rows > 0) {
         return false;
     } else {
         return true;
     }
 }
+
 
 ?>
 
